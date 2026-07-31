a4168206d4c9209e79b4954775d45f01099e5f99
glebxman <glebxxman@gmail.com>
2026-07-31 17:13:53 +0500

    feat: implement core shop, product, and settings modules with database schema and OpenApi documentation

--- src/modules/openai/openai.module.ts
+++
@@ -1,9 +0,0 @@
-import { Global, Module } from '@nestjs/common';
-import { openaiClientProvider } from './openai-client.provider';
-
-@Global()
-@Module({
-  providers: [openaiClientProvider],
-  exports: [openaiClientProvider],
-})
-export class OpenAiModule {}
--- src/modules/redis/redis.module.ts
+++
@@ -1,9 +0,0 @@
-import { Global, Module } from '@nestjs/common';
-import { redisClientProvider } from './redis-client.provider';
-
-@Global()
-@Module({
-  providers: [redisClientProvider],
-  exports: [redisClientProvider],
-})
-export class RedisModule {}
